ceea1ab0d5206ccd6b04347261a4f74ca31c51d0
lpkpaco <lpkpaco@gmail.com>
2025-08-29 19:00:20 +0800

    Added inference.py and request.py. Issue(s): Inferencing not using cuda but using cpu instead

---
+++ inference.py
@@ -0,0 +1,65 @@
+try:
+    from request import apiinfer, changeGPT, changeSoVITS
+    import torch
+except:
+    print("Missing function. Download the required files.")
+    exit()
+try:
+    import subprocess
+    import time
+    import os
+except:
+    print("Subprocess and Time modules not found.")
+    exit()
+print(str(torch.cuda.is_available()))
+print("Starting backend server. Takes around 20 seconds")
+foldername = r"D:\GPT-SoVITS\api_v2.py" #Paste the directory name of the decompressed GPT-SoVITS directory here. Please use full path. (If the folder name is xxx, then enter \xxx)
+url = "http://127.0.0.1:9880/"
+command = str("python " + foldername + r"\api_v2.py -Xfrozen_modules=off -d cuda -a 127.0.0.1 -p 9880 -c " + foldername + r"\GPT_SoVITS/configs/tts_infer.yaml")
+try: 
+    print("Starting")
+    backend = subprocess.Popen(["python", foldername], shell=False, cwd=os.path.dirname(foldername))
+    time.sleep(20)
+except:
+    print("Error when trying to start backend inference server")
+    exit()
+print(f"Backend inference server started with PID {backend.pid}")
+cmdlist = """
+1. Change model (Must perform if this is your first time running the script)
+2. Change character (Perform if this script has just been started)
+3. Run inference
+4. Exit
+"""
+character = ""
+characterlist = """
+Hitori Gotoh -> gotoh
+Ikuyo Kita -> kita
+Nijika Ichiji - > nijika
+Ryo Yamada -> ryo
+
+Enter character: 
+"""
+language = ""
+while True:
+    print(cmdlist)
+    resp = str(input("Select action: "))
+    if resp == "1":
+        gmodelpath = input("Enter the full GPT model path (e.g. D:/GPT-SoVITS/GPT_weights_v4/gotoh-v1-3-1.ckpt): ")
+        smodelname = input("Enter the full SoVITS model name (e.g. D:/GPT-SoVITS/SoVITS_weights_v4/gotoh-v1-3-1.pth): ")
+        gchange = changeGPT(gmodelpath, url)
+        print(gchange)
+        schange = changeSoVITS(smodelname, url)
+        print(schange)
+        continue
+    elif resp == "2":
+        character = str(input(characterlist))
+    elif resp == "3":
+        reqtext = input(str("Input the text (This script only supports Japanese for now): "))
+        apiresp = apiinfer(reqtext, "ja", character, url)
+        print(apiresp)
+        continue
+    elif resp == "4":
+        exit()
+    else:
+        print("Unrecognized response")
+        continue
---
+++ request.py
@@ -0,0 +1,72 @@
+def apiinfer(text, lang, char, url):
+    try:
+        import requests
+    except:
+        pass
+    if char == "gotoh":
+        audiopath = r"H:\github\BTR_GPT-SoVITS_Voicemodels\models\Hitori_Gotoh\reference_audio\人様の前で演奏できるように毎日6時間練習を続けた結果.wav" #Replace with your full path
+        prompt = "人様の前で演奏できるように毎日6時間練習を続けた結果"
+    elif char == "nijika":
+        audiopath = r"H:\github\BTR_GPT-SoVITS_Voicemodels\models\Ichiji_Nijika\reference_audio\私、下北沢高校2年、いちちにじか.wav" #Replace with your full path
+        prompt = "私、下北沢高校2年、いちちにじか"
+    else:
+        return("Character not recognised")
+    ttsurl = url + "/tts"
+    payload = {
+        "text": text,                  
+        "text_lang": lang,               
+        "ref_audio_path": audiopath,         
+        "aux_ref_audio_paths": [],    
+        "prompt_text": prompt,            
+        "prompt_lang": "ja",            
+        "top_k": 15,                   
+        "top_p": 0.8,                   
+        "temperature": 0.7,             
+        "text_split_method": "cut0",  
+        "batch_size": 1,             
+        "batch_threshold": 0.75,      
+        "split_bucket": True,         
+        "speed_factor":1.0,           
+        "streaming_mode": False,     
+        "seed": -1,                  
+        "parallel_infer": True,       
+        "repetition_penalty": 1.35,   
+        "sample_steps": 32,           
+        "super_sampling": False       
+    }
+    response = requests.post(ttsurl, json=payload)
+    if response.status_code == 200:
+        with open(r"output.wav", "wb") as f:
+            f.write(response.content)
+            f.close()
+        print("Audio generated and saved to output.wav")
+        return("Action completed")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return("Unexpected Error")
+def changeGPT(pathname, url):
+    try:
+        import requests
+    except:
+        pass
+    cmd = str(url) + "set_gpt_weights?weights_path=" + str(pathname)
+    response = requests.get(cmd)
+    if response.status_code == 200:
+        print("Changed GPT model")
+        return("Action completed")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return("Unexpected Error")
+def changeSoVITS(pathname, url):
+    try:
+        import requests
+    except:
+        pass
+    cmd = str(url) + "set_sovits_weights?weights_path=" + str(pathname)
+    response = requests.get(cmd)
+    if response.status_code == 200:
+        print("Changed SoVITS model")
+        return("Action completed")
+    else:
+        print(f"Error: {response.status_code} - {response.text}")
+        return("Unexpected Error")
