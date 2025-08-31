e3b779e4880c363344c8cdcc81c09f85bf063549
lpkpaco <lpkpaco@gmail.com>
2025-08-31 22:24:05 +0800

    Added direct model selection list. Created request_webui.py

--- web_ui.py
+++ web_ui.py
@@ -1,16 +1,56 @@
 import gradio as gr
 from inference import start_backend
-from request import apiinfer, changeGPT, changeSoVITS
-from subprocess import run
-from platform import system
+from request_webui import apiinfer, changeGPT, changeSoVITS
+import os
+from pathlib import Path
+dir_char_full = ""
+dir_char = ""
+current_path = str(Path(__file__).resolve().parent)
 print("Starting user interface. Importing modules and starting backend inferencing server.")
 start_backend()
 url = "http://127.0.0.1:9880/"
 print("Backend server started. Compiling user interface.")
+def list_gpt(char):
+    if char == "gotoh":
+        dir_char = "Hitori_Gotoh/"
+    elif char == "kita":
+        dir_char = "Ikuyo_Kita/"
+    elif char == "nijika":
+        dir_char = "Ichiji_Nijika/"
+    else:
+        return "Error"
+    dir_char = "active/" + dir_char
+    GPT_files = [f for f in os.listdir(dir_char) if f.endswith(".ckpt")]
+    GPT_files.sort()
+    return GPT_files
+def list_sovits(char):
+    if char == "gotoh":
+        dir_char = "Hitori_Gotoh/"
+    elif char == "kita":
+        dir_char = "Ikuyo_Kita/"
+    elif char == "nijika":
+        dir_char = "Ichiji_Nijika/"
+    else:
+        return "Error"
+    dir_char = "active/" + dir_char
+    SoVITS_files = [f for f in os.listdir(dir_char) if f.endswith(".pth")]
+    SoVITS_files.sort()
+    return SoVITS_files
 def set_url(url_value):
     url = url_value
     return ("URL changed to " + url)
+def refresh_dropdowns_GPT(char):
+    filelist = list_gpt(char)
+    return gr.Dropdown(
+        choices = filelist
+    )
+def refresh_dropdowns_SoVITS(char):
+    filelist = list_sovits(char)
+    return gr.Dropdown(
+        choices = filelist
+    )
 global url_input
+global char_input
 ui_lang = input("""
 Select language:
 English -> en_US
@@ -20,6 +60,9 @@ Enter:
 """)
 if ui_lang == "en_US":
     with gr.Blocks() as webui:
+        with gr.Group():
+            gr.Markdown("Web User Interface Tool")
+            gr.Image(type = "filepath", value = "asset/readme/b.gif")
         with gr.Group():
             url_input = gr.Textbox(label = "Enter backend server url (For default users, do not enter anything.)", value="http://127.0.0.1:9880/")
             button = gr.Button("Change backend server url")
@@ -42,20 +85,43 @@ if ui_lang == "en_US":
                 outputs = [audio, status]
             )
         with gr.Group():
-            gpt_input = gr.Textbox(label = "Enter GPT model full path")
+            gpt_input = gr.Dropdown(
+                choices = [],
+                label = "Select GPT model from list"
+            )
+            refresh = gr.Button("Refresh list")
             button = gr.Button("Change GPT model")
             status = gr.Textbox(label = "Output")
-            button.click(fn = changeGPT, inputs = [gpt_input, url_input], outputs = status)
+            hidden_path = gr.Textbox(value=current_path, visible=False)
+            button.click(fn = changeGPT, inputs = [gpt_input, url_input, char_input, hidden_path], outputs = status)
+            refresh.click(
+                fn = refresh_dropdowns_GPT,
+                inputs = char_input,
+                outputs = gpt_input
+            )
         with gr.Group():
-            gpt_input = gr.Textbox(label = "Enter SoVITS model full path")
+            sovits_input = gr.Dropdown(
+                choices = [],
+                label = "Select SoVITS model from list"
+            )
+            refresh = gr.Button("Refresh list")
             button = gr.Button("Change SoVITS model")
             status = gr.Textbox(label = "Output")
-            button.click(fn = changeSoVITS, inputs = [gpt_input, url_input], outputs = status)
-    webui.launch(server_name = "0.0.0.0", server_port = 8964, share=True)
+            hidden_path = gr.Textbox(value=current_path, visible=False)
+            button.click(fn = changeSoVITS, inputs = [sovits_input, url_input, char_input, hidden_path], outputs = status)
+            refresh.click(
+                fn = refresh_dropdowns_SoVITS,
+                inputs = char_input,
+                outputs = sovits_input
+            )
+    webui.launch(server_name = "0.0.0.0", server_port = 8100)
     print("User interface loaded. Please open it in your browser.")
 
 elif ui_lang == "zh_Hant":
     with gr.Blocks() as webui:
+        with gr.Group():
+            gr.Markdown("Web User Interface Tool")
+            gr.Image(type = "filepath", value = "asset/readme/b.gif")
         with gr.Group():
             url_input = gr.Textbox(label = "後端伺服器地址（如使用預設設置請勿變更）", value="http://127.0.0.1:9880/")
             button = gr.Button("變更")
@@ -78,16 +144,36 @@ elif ui_lang == "zh_Hant":
                 outputs = [audio, status]
             )
         with gr.Group():
-            gpt_input = gr.Textbox(label = "輸入GPT模型的full path")
+            gpt_input = gr.Dropdown(
+                choices = [],
+                label = "從選單中選擇GPT模型"
+            )
+            refresh = gr.Button("更新模型清單")
             button = gr.Button("變更GPT模型")
             status = gr.Textbox(label = "狀態")
-            button.click(fn = changeGPT, inputs = [gpt_input, url_input], outputs = status)
+            hidden_path = gr.Textbox(value=current_path, visible=False)
+            button.click(fn = changeGPT, inputs = [gpt_input, url_input, char_input, hidden_path], outputs = status)
+            refresh.click(
+                fn = refresh_dropdowns_GPT,
+                inputs = char_input,
+                outputs = gpt_input
+            )
         with gr.Group():
-            gpt_input = gr.Textbox(label = "輸入SoVITS模型的full path")
+            sovits_input = gr.Dropdown(
+                choices = [],
+                label = "從選單中選擇SoVITS模型"
+            )
+            refresh = gr.Button("更新模型清單")
             button = gr.Button("變更SoVITS模型")
             status = gr.Textbox(label = "狀態")
-            button.click(fn = changeSoVITS, inputs = [gpt_input, url_input], outputs = status)
-    webui.launch(server_name = "0.0.0.0", server_port = 8964, share=True)
+            hidden_path = gr.Textbox(value=current_path, visible=False)
+            button.click(fn = changeSoVITS, inputs = [sovits_input, url_input, char_input, hidden_path], outputs = status)
+            refresh.click(
+                fn = refresh_dropdowns_SoVITS,
+                inputs = char_input,
+                outputs = sovits_input
+            )
+    webui.launch(server_name = "0.0.0.0", server_port = 8100)
     print("使用者頁面完成加載。請在瀏覽器中打開。")
 
 else:
